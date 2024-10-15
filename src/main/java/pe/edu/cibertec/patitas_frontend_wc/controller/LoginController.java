41e2b04b0fae5d99a441c8e658bec1f1ba52a1c8
bryandevl <165954093+bryandevl@users.noreply.github.com>
2024-10-15 00:45:07 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginController.java
@@ -0,0 +1,31 @@
+package pe.edu.cibertec.patitas_frontend_wc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc.viewmodel.LoginModel;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    WebClient webClientAutenticacion;
+
+    @GetMapping("/inicio")
+    public String inicio(Model model) {
+        LoginModel loginModel = new LoginModel("00", "", "");
+        model.addAttribute("loginModel", loginModel);
+        return "inicio";
+    }
+
+}
